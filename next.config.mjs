ba98eb71825c6838c3e417afe24253357153f866
최하영 <xxhayoxx@naver.com>
2024-07-26 13:39:13 +0900

    test: next image blur 관련 패키지 제거

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,22 @@
-import withPlaiceholder from '@plaiceholder/next';
-import withImages from "next-images";
-
 /** @type {import('next').NextConfig} */
-const nextConfig = withImages({
-    //distDir: "build",
-    staticPageGenerationTimeout: 300,
-    images: {
-      domains: [
-        'www.notion.so',
-        'notion.so',
-        'images.unsplash.com',
-        'pbs.twimg.com',
-        'abs.twimg.com',
-        's3.us-west-2.amazonaws.com',
-        'transitivebullsh.it',
-        'prod-files-secure.s3.us-west-2.amazonaws.com',
-        'lh3.googleusercontent.com'
-      ],
-      formats: ['image/avif', 'image/webp'],
-      dangerouslyAllowSVG: true,
-      contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
-    }
-});
+const nextConfig = {
+  staticPageGenerationTimeout: 300,
+  images: {
+    domains: [
+      'www.notion.so',
+      'notion.so',
+      'images.unsplash.com',
+      'pbs.twimg.com',
+      'abs.twimg.com',
+      's3.us-west-2.amazonaws.com',
+      'transitivebullsh.it',
+      'prod-files-secure.s3.us-west-2.amazonaws.com'
+    ],
+    formats: ['image/avif', 'image/webp'],
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
+  }
+};
 
-export default withPlaiceholder(nextConfig);
+export default nextConfig;
 
